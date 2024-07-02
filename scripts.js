a02742132b0b7f8899065bc554c1fe8118d6ef3a
whytap0801 <whytap68@gmail.com>
2024-07-02 12:23:25 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,27 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+      
+
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import {getAuth} from "firebase/auth"
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDvjZBtNA8KLQd6N5ydFjCNCc5JCz7o6Eg",
+//   authDomain: "login-fc955.firebaseapp.com",
+//   projectId: "login-fc955",
+//   storageBucket: "login-fc955.appspot.com",
+//   messagingSenderId: "813997185248",
+//   appId: "1:813997185248:web:22f0815d266e0b4377d885",
+//   measurementId: "G-01T2ES5S5B"
+// };
+
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+   
